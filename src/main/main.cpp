4d37eec560d4b87f34c4cf12fd0ec1f78652a81d
Anthony Williams <tonywilliamspiano@gmail.com>
2023-08-17 12:19:48 +0200

    chatGPT hello world program

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -12,7 +12,55 @@
 
 #include "Server.hpp"
 
-int	main()
-{
-	Server	server;
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+const int PORT = 80;
+
+int main() {
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (serverSocket == -1) {
+		std::cerr << "Error creating socket." << std::endl;
+		return 1;
+	}
+
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+	serverAddress.sin_port = htons(PORT);
+
+	if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+		std::cerr << "Error binding to port " << PORT << "." << std::endl;
+		return 1;
+	}
+
+	if (listen(serverSocket, 10) == -1) {
+		std::cerr << "Error listening on socket." << std::endl;
+		return 1;
+	}
+
+	std::cout << "Server is listening on port " << PORT << "." << std::endl;
+
+	while (true) {
+		int clientSocket = accept(serverSocket, NULL, NULL);
+		if (clientSocket == -1) {
+			std::cerr << "Error accepting client connection." << std::endl;
+			continue;
+		}
+
+		std::string response = "HTTP/1.1 200 OK\r\n"
+							   "Content-Type: text/html\r\n"
+							   "\r\n"
+							   "<html><body><h1>Hello, World!</h1></body></html>";
+
+		send(clientSocket, response.c_str(), response.size(), 0);
+		close(clientSocket);
+	}
+
+	close(serverSocket);
+	return 0;
 }
