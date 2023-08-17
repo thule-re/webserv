0498c3c189e1ce98160b3e552691a341da3cdc84
treeps <treeps@student.42wolfsburg.de>
2023-08-17 17:01:01 +0200

    moved everything to server Class

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -14,12 +14,18 @@
 # define WEBSERV_SERVER_HPP
 
 # include <iostream>
+# include <string>
+# include <cstring>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <unistd.h>
+# include <fstream>
 
 class Server {
 public:
 	// constructors
 	Server();
-
+	Server(int port, std::string index, std::string error, std::string folder);
 	Server(const Server &);
 
 	// destructor
@@ -29,6 +35,25 @@ public:
 	Server &operator=(const Server &);
 
 	// member functions
+	void	init();
+	void	loop();
+
+private:
+	void	handleRequest(int clientSocket);
+	void	handleGETRequest(int clientSocket, std::string request);
+	void	handlePOSTRequest(int clientSocket, std::string request);
+	void	handleDELETERequest(int clientSocket, std::string request);
+	void	handleInvalidRequest(int clientSocket);
+
+	std::string extractPath(std::string request, int start);
+	std::string getContentType(std::string path);
+	std::string getErrorPage();
+
+	int			_port;
+	int			_serverSocket;
+	std::string	_indexPath;
+	std::string	_errorPath;
+	std::string	_indexFolder;
 };
 
 #endif
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -10,142 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <fstream>
+#include "Server.hpp"
 
-const int PORT = 80;
-std::string indexPath = "garbage.html";
-std::string errorPath = "error404.html";
-std::string indexFolder = "www";
+int main(int argc, char **argv) {
+	Server server;
 
-std::string extractPath(std::string request, int start) {
-	std::string path = request.substr(start, request.find("HTTP/1.1") - 5);
-	if (path == "/") {
-		path += indexPath;
-	}
-	return path;
-}
-
-std::string getContentType(std::string path) {
-	if (path.find(".html") != std::string::npos) {
-		return "text/html";
-	} else if (path.find(".css") != std::string::npos) {
-		return "text/css";
-	} else if (path.find(".js") != std::string::npos) {
-		return "text/javascript";
-	} else if (path.find(".png") != std::string::npos) {
-		return "image/png";
-	} else if (path.find(".jpg") != std::string::npos) {
-		return "image/jpeg";
-	} else if (path.find(".gif") != std::string::npos) {
-		return "image/gif";
-	} else if (path.find(".ico") != std::string::npos) {
-		return "image/x-icon";
+	if (argc == 5) {
+		std::cout << "debug" << std::endl;
+		server = Server(atoi(argv[1]), argv[2], argv[3], argv[4]);
 	} else {
-		return "text/plain";
+		server = Server();
 	}
-}
-
-std::string getErrorPage() {
-	std::ifstream errorFile((indexFolder + "/" + errorPath).c_str());
-	if (!errorFile.good()) {
-		std::cerr << "Error opening error file. Using Default" << std::endl;
-		return "<html><body><h1>Default 404 Not Found</h1></body></html>";
-	}
-	std::string line;
-	std::string errorPage;
-	while (std::getline(errorFile, line)) {
-		errorPage += line;
-	}
-	return errorPage;
-}
-
-void handleGetRequest(int clientSocket, std::string request) {
-	std::string response;
-	std::string path = (indexFolder + extractPath(request, 4));
-	std::string contentType = getContentType(path);
-	std::cout << "Valid request.\nPath: " << path << "\nContent-Type: " << contentType << std::endl << std::endl;
-	std::ifstream file(path.c_str());
-	std::string line;
-	if (file.good()) {
-		response = "HTTP/1.1 200 OK\r\nContent-Type: " + contentType + "\r\n\r\n";
-		while (std::getline(file, line)) {
-			response += line;
-		}
-	} else {
-		std::cerr << "File not found." << std::endl << std::endl;
-		response = "HTTP/1.1 404 Not Found\r\n"
-				   "Content-Type: text/html \r\n"
-				   "\r\n";
-		response += getErrorPage();
-	}
-	send(clientSocket, response.c_str(), response.size(), 0);
-	close(clientSocket);
-}
-
-int main() {
-	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (serverSocket == -1) {
-		std::cerr << "Error creating socket." << std::endl;
-		return 1;
-	}
-
-	sockaddr_in serverAddress;
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_addr.s_addr = INADDR_ANY;
-	serverAddress.sin_port = htons(PORT);
-
-	if (bind(serverSocket, (struct sockaddr *) &serverAddress,
-			 sizeof(serverAddress)) == -1) {
-		std::cerr << "Error binding to port " << PORT << "." << std::endl;
-		return 1;
-	}
-
-	if (listen(serverSocket, 10) == -1) {
-		std::cerr << "Error listening on socket." << std::endl;
-		return 1;
-	}
-
-	std::cout << "Server is listening on port " << PORT << "." << std::endl;
-
-	while (true) {
-		int clientSocket = accept(serverSocket, NULL, NULL);
-		if (clientSocket == -1) {
-			std::cerr << "Error accepting client connection." << std::endl;
-			continue;
-		}
-
-		// Read the request from the client
-		char buffer[1024];
-		ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-		if (bytesRead == -1) {
-			std::cerr << "Error reading from client socket." << std::endl;
-			close(clientSocket);
-			continue;
-		}
-
-		// Print the received request
-		std::cout << "Received Request:\n" << std::string(buffer, bytesRead)
-				  << std::endl;
-		std::string stringBuffer = std::string(buffer, bytesRead);
-		if (stringBuffer.find("GET ") == 0) {
-			handleGetRequest(clientSocket, stringBuffer);
-		} else {
-			std::cerr << "Invalid request." << std::endl;
-			std::string response = "HTTP/1.1 400 OK\r\n"
-								   "Content-Type: text/html\r\n"
-								   "\r\n"
-								   "<html><body><h1>400 Bad Request</h1></body></html>";
-			send(clientSocket, response.c_str(), response.size(), 0);
-			close(clientSocket);
-		}
-	}
-
-	close(serverSocket);
-	return 0;
+	server.init();
+	server.loop();
+	return (0);
 }
