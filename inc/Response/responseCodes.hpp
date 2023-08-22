1fbe9c2f39efc5f054bfa0e517e297b801ef26b1
treeps <treeps@student.42wolfsburg.de>
2023-08-22 13:53:10 +0200

    small refactor

--- inc/Response/responseCodes.hpp
+++ inc/Response/responseCodes.hpp
@@ -13,11 +13,15 @@
 #ifndef WEBSERV_RESPONSECODES_HPP
 # define WEBSERV_RESPONSECODES_HPP
 
+// HTTP Status Codes
+// 2xx Success
 # define OK 200
 # define CREATED 201
 # define ACCEPTED 202
 # define NO_CONTENT 204
 # define PARTIAL_CONTENT 206
+
+// 3xx Redirection
 # define MULTIPLE_CHOICES 300
 # define MOVED_PERMANENTLY 301
 # define FOUND 302
@@ -25,6 +29,8 @@
 # define NOT_MODIFIED 304
 # define USE_PROXY 305
 # define TEMPORARY_REDIRECT 307
+
+// 4xx Client Error
 # define BAD_REQUEST 400
 # define UNAUTHORIZED 401
 # define PAYMENT_REQUIRED 402
@@ -43,6 +49,8 @@
 # define UNSUPPORTED_MEDIA_TYPE 415
 # define REQUESTED_RANGE_NOT_SATISFIABLE 416
 # define EXPECTATION_FAILED 417
+
+// 5xx Server Error
 # define INTERNAL_SERVER_ERROR 500
 # define NOT_IMPLEMENTED 501
 # define BAD_GATEWAY 502
@@ -50,11 +58,15 @@
 # define GATEWAY_TIMEOUT 504
 # define HTTP_VERSION_NOT_SUPPORTED 505
 
+// HTTP Error Messages
+// 2xx Success
 # define MSG_200 "OK"
 # define MSG_201 "Created"
 # define MSG_202 "Accepted"
 # define MSG_204 "No Content"
 # define MSG_206 "Partial Content"
+
+// 3xx Redirection
 # define MSG_300 "Multiple Choices"
 # define MSG_301 "Moved Permanently"
 # define MSG_302 "Found"
@@ -62,6 +74,8 @@
 # define MSG_304 "Not Modified"
 # define MSG_305 "Use Proxy"
 # define MSG_307 "Temporary Redirect"
+
+// 4xx Client Error
 # define MSG_400 "Bad Request"
 # define MSG_401 "Unauthorized"
 # define MSG_402 "Payment Required"
@@ -80,6 +94,8 @@
 # define MSG_415 "Unsupported Media Type"
 # define MSG_416 "Requested Range Not Satisfiable"
 # define MSG_417 "Expectation Failed"
+
+// 5xx Server Error
 # define MSG_500 "Internal Server Error"
 # define MSG_501 "Not Implemented"
 # define MSG_502 "Bad Gateway"
--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -13,28 +13,19 @@
 #include "Server/Server.hpp"
 
 // constructors
-Server::Server(): _port(80), _indexPath("garbage.html"), _errorPath("error404.html"), _indexFolder("www") {
-	std::cout << "Server default constructor called" << std::endl;
-}
+Server::Server(): _port(80), _serverSocket(), _indexPath("garbage.html"), _errorPath("error404.html"), _indexFolder("www") {}
 
-Server::Server(int port, const std::string& index, const std::string& error, const std::string& folder): _port(port), _indexPath(index), _errorPath(error), _indexFolder(folder) {
-	std::cout << "Server parameter constructor called" << std::endl;
-}
+Server::Server(int port, const std::string& index, const std::string& error, const std::string& folder): _port(port), _serverSocket(), _indexPath(index), _errorPath(error), _indexFolder(folder) {}
 
-Server::Server(const Server &other) {
-	std::cout << "Server copy constructor called" << std::endl;
+Server::Server(const Server &other) : _port(), _serverSocket() {
 	*this = other;
 }
 
 // destructor
-Server::~Server() {
-	std::cout << "Server destructor called" << std::endl;
-}
+Server::~Server() {}
 
 // operator overload
 Server &Server::operator=(const Server &other) {
-
-	std::cout << "Server assignment operator called" << std::endl;
 	if (this == &other)
 		return (*this);
 	_port = other._port;
@@ -81,7 +72,6 @@ void	Server::loop() {
 			std::cerr << "Error accepting connection" << std::endl;
 			exit(1);
 		}
-		std::cout << "Connection accepted" << std::endl;
 		handleRequest(clientSocket);
 	}
 }
@@ -102,137 +92,4 @@ void Server::handleRequest(int clientSocket) {
 	response.send();
 	client.closeSocket();
 	delete request;
-}
-
-//void Server::handleRequest(int clientSocket) {
-//	char buffer[BUFFER_SIZE];
-//	ssize_t bytesRead;
-//	std::string stringBuffer;
-//
-//	while (true)
-//	{
-//		bytesRead =  recv(clientSocket, buffer, BUFFER_SIZE, 0);
-//		if (bytesRead < 0) {
-//			std::cerr << "Error reading from client socket" << std::endl;
-//			exit(1);
-//		}
-//		stringBuffer += std::string(buffer, bytesRead);
-//		if (bytesRead < BUFFER_SIZE) {
-//			break;
-//		}
-//	}
-//	if (stringBuffer.find("GET ") == 0) {
-//		handleGETRequest(clientSocket, stringBuffer);
-//	} else if (stringBuffer.find("POST ") == 0) {
-//		handlePOSTRequest(clientSocket, stringBuffer);
-//	} else if (stringBuffer.find("DELETE ") == 0) {
-//		handleDELETERequest(clientSocket, stringBuffer);
-//	} else {
-//		handleInvalidRequest(clientSocket);
-//	}
-//	close(clientSocket);
-//}
-
-void Server::handleGETRequest(int clientSocket, const std::string& request) {
-	std::string path = extractPath(request, 4);
-	if (path == "/") {
-		path += _indexPath;
-	}
-	path = _indexFolder + path;
-	std::string response;
-	std::ifstream file(path.c_str());
-
-	std::cout << "GET request for file: " << path << std::endl;
-
-	if (file.is_open()) {
-		response = "HTTP/1.1 200 OK\r\n"
-				   "Content-Type: " + getContentType(path) + "\r\n"
-				   "\r\n";
-		std::string line;
-		while (getline(file, line)) {
-			response += line + "\n";
-		}
-	} else {
-		response = "HTTP/1.1 404 Not Found\r\n"
-				   "Content-Type: text/html \r\n"
-				   "\r\n";
-		response += getErrorPage();
-	}
-	send(clientSocket, response.c_str(), response.size(), 0);
-}
-
-void Server::handlePOSTRequest(int clientSocket, const std::string& request) {
-	(void) clientSocket;
-	(void) request;
-	// TODO: implement
-}
-
-void Server::handleDELETERequest(int clientSocket, const std::string& request) {
-	std::string path = _indexFolder + extractPath(request, 7);
-	std::string response;
-	if (path == "/") {
-		path += _indexPath;
-	}
-	path = _indexFolder + path;
-
-	std::cout << "Deleting file: " << path << std::endl;
-
-	if (std::remove(path.c_str()) == 0) {
-		response = "HTTP/1.1 200 OK\r\n"
-				   "Content-Type: text/html \r\n"
-				   "\r\n"
-				   "<html><body><h1>File deleted</h1></body></html>";
-	} else {
-		response = "HTTP/1.1 404 Not Found\r\n"
-				   "Content-Type: text/html \r\n"
-				   "\r\n";
-		response += getErrorPage();
-	}
-	send(clientSocket, response.c_str(), response.size(), 0);
-}
-
-void Server::handleInvalidRequest(int clientSocket) {
-	std::string response = "HTTP/1.1 400 Bad ARequest\r\n"
-						   "Content-Type: text/html \r\n"
-						   "\r\n";
-	response += getErrorPage();
-	send(clientSocket, response.c_str(), response.size(), 0);
-}
-
-std::string Server::extractPath(const std::string& request, int start) {
-	std::string path = request.substr(start, request.find("HTTP/1.1") - (start + 1));
-	return path;
-}
-
-std::string Server::getContentType(const std::string& path) {
-	if (path.find(".html") != std::string::npos) {
-		return "text/html";
-	} else if (path.find(".css") != std::string::npos) {
-		return "text/css";
-	} else if (path.find(".js") != std::string::npos) {
-		return "text/javascript";
-	} else if (path.find(".png") != std::string::npos) {
-		return "image/png";
-	} else if (path.find(".jpg") != std::string::npos) {
-		return "image/jpeg";
-	} else if (path.find(".gif") != std::string::npos) {
-		return "image/gif";
-	} else if (path.find(".ico") != std::string::npos) {
-		return "image/x-icon";
-	} else {
-		return "text/plain";
-	}
-}
-
-std::string Server::getErrorPage() {
-	std::ifstream errorFile((_indexFolder + "/" + _errorPath).c_str());
-	if (!errorFile.good()) {
-		return DEFAULT_404;
-	}
-	std::string line;
-	std::string errorPage;
-	while (std::getline(errorFile, line)) {
-		errorPage += line;
-	}
-	return errorPage;
 }
