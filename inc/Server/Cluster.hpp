34582ab1fa404c65918de6e639f8550b7413d359
Anthony Williams <tonywilliamspiano@gmail.com>
2023-09-14 13:15:09 +0200

    add cluster class

---
+++ inc/Server/Cluster.hpp
@@ -0,0 +1,51 @@
+
+#ifndef WEBSERV_CLUSTER_HPP
+#define WEBSERV_CLUSTER_HPP
+
+#include <iostream>
+#include <map>
+#include "Server.hpp"
+#include "../Socket/ClientSocket.hpp"
+
+
+class Cluster {
+public:
+	// constructors
+	Cluster();
+
+	Cluster(const Cluster &);
+
+	// destructor
+	~Cluster();
+
+	// operator overload
+	Cluster &operator=(const Cluster &other);
+
+	//member functions
+	void initializeServers();
+
+	void selectClientSockets();
+
+	void loop();
+
+	void addClientToMap(ClientSocket clientSocket);
+
+
+private:
+	std::map<int, Server> _serverMap;
+	std::map<int, ClientSocket> _clientsMap;
+
+	fd_set		_readSet;
+	fd_set		_writeSet;
+	fd_set		_readSetCopy;
+	fd_set		_writeSetCopy;
+
+	int _maxFd;
+
+	void handleLoopException(std::exception &exception);
+
+	void closeConnection(ClientSocket socket);
+};
+
+
+#endif //WEBSERV_CLUSTER_HPP
